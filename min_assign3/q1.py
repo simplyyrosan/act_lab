6202e585b24bbdcd4778050da93a4660a13f3269
simplyyrosan <ommprakash825@gmail.com>
2025-11-29 20:54:39 +0530

    Q1 fixed

--- min_assign3/q1.py
+++ min_assign3/q1.py
@@ -1,6 +1,7 @@
-def showEven(min, max):
-    for i in range(min, max+1):
-        if i%2==0:
-            print(i)
+min = int(input("Enter the minimum range: "))
+max = int(input("Enter the maximum range: "))
+print("Even number within the range are: ")
+for i in range(min, max+1):
+    if i%2==0:
+        print(i, end = " ")
             
-showEven(1, 11)
