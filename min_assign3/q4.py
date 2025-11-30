4ad234bc18f767767146a2a35958aacc26670a9e
simplyyrosan <ommprakash825@gmail.com>
2025-11-30 12:00:46 +0530

    changes

--- min_assign3/q4.py
+++ min_assign3/q4.py
@@ -1,2 +1,10 @@
+# i = 10
+# while i > 0:
+#   print( i )
+#   i −= 2
+#Convert to for loop
+
+
+
 for i in range(10, 1, -2):
     print(i)
--- min_assign3/q6.py
+++ min_assign3/q6.py
@@ -2,3 +2,5 @@ for i in range(10):
     if i%3 == 0:
         continue 
     print(i, end=' ')
+
+#Output: 1 2 4 5 7 8 
